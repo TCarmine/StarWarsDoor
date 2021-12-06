045eaf2639838bf9c7d0057da177f496a81817e7
TCarmine <kappa85t@hotmail.it>
2021-12-06 11:26:44 +0100

    basic controlled form and axios installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [registerUsername, setRegisterUsername] = useState("")
+  const [registerPassword, setRegisterPassword] = useState("")
+  const [loginUsername, setLoginUsername] = useState("")
+  const [loginPassword, setLoginPassword] = useState("")
+
+  const registerUser = () =>{} 
+  const loginUser = () =>{}
+  const getUser = () =>{}   
+
   return (
     <div className="App">
+      <div>
+        <h1>Register</h1>
+        <input type="text" placeholder="username" onChange={e =>setRegisterUsername(e.target.value)} />
+        <input type="text" placeholder="password" onChange={e =>setRegisterPassword(e.target.value)} />
+        <button onClick={registerUser}>Submit</button>
+      </div> 
       
+      <div>
+        <h1>Login</h1>
+        <input type="text" placeholder="username" onChange={e =>setLoginUsername(e.target.value)} />
+        <input type="text" placeholder="password" onChange={e =>setLoginPassword(e.target.value)} />
+        <button onClick={loginUser}>Submit</button>
+      </div> 
+
+      <div>
+        <h1>Get User</h1>
+        <button onClick={getUser}>Submit</button>
+      </div>
     </div>
   );
 }
