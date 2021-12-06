8294cc3422803adf01923a7944257fd6f361dc36
TCarmine <kappa85t@hotmail.it>
2021-12-06 09:32:00 +0100

    basic routers, middlware

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,42 @@ const passport = require('passport')
 const passportLocal = require('passport-local').Strategy
 const cookieParser = require(cookie-parser)
 const bcrypt = require('bcryptjs')
-const expressSession = require('express-session')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 
-const App = express()
+const app = express()
 
 // Middleware
-App.use(bodyParser.json())
-App.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+  origin:'http://localhost:300', // url where the react app connect to
+  credentials:true
+}))
 
-App.listen(4000, ()=>{
+app.use(session({
+  secret:"secretcode",
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(cookieParser("secretcode"))
+
+// Routers
+app.post("/login",(req, res) =>{
+  console.log(req.body)
+})
+
+app.post("/register",(req, res) =>{
+  console.log(req.body)
+})
+
+app.get("/user",(req, res) =>{
+
+})
+
+
+
+app.listen(4000, ()=>{
   console.log('Server has started')
 })
