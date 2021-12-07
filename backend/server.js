f37fbb6d937ae8cfa1eddf3fe69e8b9c7ee113e9
TCarmine <kappa85t@hotmail.it>
2021-12-07 09:00:08 +0100

    add mongo connection string and configuration of register in FE

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,24 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+//DB connetion
+mongoose.connect(
+  "mongodb+srv://Carmine:%24vetere02468T%21%3F@cluster0.md02r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+  },
+  ()=>{
+    console.log("Mongoose is connected")
+  }
+)
+
+
 // Middleware
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors({
-  origin:'http://localhost:300', // url where the react app connect to
+  origin:'http://localhost:3000', // url where the react app connect to
   credentials:true
 }))
 
@@ -35,12 +48,12 @@ app.post("/register",(req, res) =>{
   console.log(req.body)
 })
 
-app.get("/user",(req, res) =>{
+app.get("/getUser",(req, res) =>{
 
 })
 
 
 
-app.listen(4000, ()=>{
+app.listen(4000,() =>{
   console.log('Server has started')
 })
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import './App.css';
+import './App.css'
+import axios from 'axios'
 
 function App() {
   const [registerUsername, setRegisterUsername] = useState("")
@@ -7,9 +8,35 @@ function App() {
   const [loginUsername, setLoginUsername] = useState("")
   const [loginPassword, setLoginPassword] = useState("")
 
-  const registerUser = () =>{} 
-  const loginUser = () =>{}
-  const getUser = () =>{}   
+  const registerUser = () =>{
+    axios({
+      method:"POST",
+      data: {
+        username: registerUsername,
+        password: setRegisterPassword,
+      },
+      withCredentials: true,
+      url:"http://localhost:4000/register",
+    }).then( res => console.log(res))
+  } 
+  const loginUser = () =>{
+    axios({
+      method:"POST",
+      data: {
+        username: loginUsername,
+        password: setLoginPassword,
+      },
+      withCredentials: true,
+      url:"http://localhost:4000/login",
+    }).then( res => console.log(res))
+  }
+  const getUser = () =>{
+    axios({
+      method:"GET",
+      withCredentials: true,
+      url:"http://localhost:4000/getUser",
+    }).then( res => console.log(res))
+  }   
 
   return (
     <div className="App">
