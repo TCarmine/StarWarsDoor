b8b4b6abbdd10ede91f50d6dd44605182cb65f06
TCarmine <kappa85t@hotmail.it>
2021-12-09 14:29:49 +0100

    missing react router dom and basic hooks for api

--- client/src/components/StarWars/StarsCallAPI.js
+++ client/src/components/StarWars/StarsCallAPI.js
@@ -1,1 +1,40 @@
 
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const rootUrl = 'https://swapi.dev/api/'
+
+const films = 'people'
+
+const  GithubContext = React.createContext()
+
+// Provide, Consumer - GithubContext.Provider
+
+const GithubProvider = ({children})=>{
+  // request loading
+  const [request, setRequest] = useState(0)
+  const [loading, setIsLoading] = useState(false)
+  
+ 
+
+  const checkRequest = () => {
+    axios({data}) =>{ 
+        console.log()
+    }).then((response)=>{
+      console.log()
+    }).catch((err)=> console.log(err))
+  }
+
+  // manage first loading
+  useEffect(()=>{
+    checkRequest()
+  },[])
+
+  return (
+    <GithubContext.Provider value={"Loading different attributes"}>
+      {children}
+    </GithubContext.Provider>
+  )  
+}
+
+export{GithubProvider, GithubContext}
