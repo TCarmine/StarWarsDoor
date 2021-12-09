97140be83258ec7a144bd415269b23649e17f7bd
TCarmine <kappa85t@hotmail.it>
2021-12-09 07:35:17 +0100

    import for API component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import Auth from './components/Auth/Auth'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App({}) {
   return (
