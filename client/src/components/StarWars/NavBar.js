f531a24c54ce1a3175007a9a97afdddb14f0bc21
TCarmine <kappa85t@hotmail.it>
2021-12-12 17:52:38 +0100

    logo and rescaled

--- client/src/components/StarWars/NavBar.js
+++ client/src/components/StarWars/NavBar.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../../star-wars-4.svg'
 
 const Navbar = () => {
   return (
-    <div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="star wars view"></img>
+        </Link>
+      </div>
       <h2>navbar component</h2>
-    </div>
+    </nav>
   )
 }
 
