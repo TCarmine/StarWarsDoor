81f9f900f66a1163b3cccbce08a656363419f896
TCarmine <kappa85t@hotmail.it>
2021-12-12 17:26:17 +0100

    structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,28 @@ import React, { useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import Auth from './components/Auth/Auth'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import NavBar from './components/StarWars/NavBar'
+import Resources from './components/StarWars/Resources'
+import About from './components/StarWars/About'
+import Error from './components/StarWars/Error'
+
+
 
 function App({}) {
   return (
     
-    <>
+   /*  <>
       <Auth />
-    </>
+    </> */
+    <Router>
+     <NavBar />
+     <Routes>
+       <Route exact path="/" element={<Resources />} />
+       <Route exact path="/about" element={<About />} />
+       <Route exact path="*" element={<Error />} />
+     </Routes>
+    </Router> 
   );
 }
 
---
+++ client/src/components/StarWars/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <div>
+      <h2>Not found page / 404</h2>
+    </div>
+  )
+}
+
+export default Error
