bc7640704c1351d012814bd2767aae68d0bbc4b4
TCarmine <kappa85t@hotmail.it>
2021-12-13 06:48:41 +0100

    all root reasources fetch

--- client/src/context.js
+++ client/src/context.js
@@ -10,13 +10,34 @@ const AppProvider = ({ children }) => {
   const [resources, setResources] = useState([])
 
   const fetchResources = async () => {
-    setLoading(true)
     try{
       const response = await fetch(`${url}${searchTerm}`) 
       const data = await response.json()
       console.log(data)
+
+      const {allList} = data
+
+      if(allList){
+        const rootList = allList.map((item)=>{
+        const { films, people, planets, species, starships, vehicles } = item
+          return { 
+            films, 
+            people, 
+            planets, 
+            species, 
+            starships, 
+            vehicles 
+          }
+        })
+        setResources([rootList])
+        
+      }else{
+        setResources([])
+      }
+      setLoading(false)
     } catch (error){
       console.log(error)
+      setLoading(false)
     }
   }
 
